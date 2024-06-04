d5e60d2c201e8b136f9268256946c8b937d736ca
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-04 20:35:51 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/bg2.png";
+import "./assets/img/d.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateDomain();
+
+  function generateDomain() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domain = [".com", ".es", ".cl", ".ar", ".net", ".io"];
+
+    let pronounIndx = Math.floor(Math.random() * pronoun.length);
+    let adjIndx = Math.floor(Math.random() * adj.length);
+    let nounIndx = Math.floor(Math.random() * noun.length);
+    let domainIndx = Math.floor(Math.random() * domain.length);
+
+    return (
+      pronoun[pronounIndx] + adj[adjIndx] + noun[nounIndx] + domain[domainIndx]
+    );
+  }
 };
